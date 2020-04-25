cee8c6d419de338ab9f632a9dce8bf4afcaad0f4
Rakesh Rawat <rrawat4@sapient.com>
2020-04-25 17:40:18 +0530

    LOG Cookies

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,6 +1,5 @@
 import passport from './passport';
 import cookieSession from 'cookie-session'
-import { route } from 'next/dist/next-server/server/router';
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
@@ -21,20 +20,15 @@ export default async function  (req, res) {
     await middleware(req, res, cookieSession({
         keys: ['aaaa'],
         secret: '123455',
-
-        secureProxy: true,
-        secure: true,
-        proxy: true,
-        cookie: {
-          secureProxy: true,
-          secure: true,
-          proxy: true
-        }
-        //httpOnly: true
+        //secure: true,
+        httpOnly: true
     }))
 
     await middleware(req, res, passport.initialize())
     await middleware(req, res, passport.session())
+
+    console.log('COOKIES: ', req.cookies);
+
 
     return {
         req, 
